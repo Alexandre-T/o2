3b2c39c49c83090b230f7bbc68d3b56964187b46
Alexandre-T <alexandre.tranchant@gmail.com>
2019-03-30 16:50:53 +0100

    Kernel syntax fixed

--- src/Kernel.php
+++ src/Kernel.php
@@ -1,20 +1,45 @@
 <?php
+/**
+ * This file is part of the O2 Application.
+ *
+ * PHP version 7.1|7.2|7.3|7.4
+ *
+ * (c) Alexandre Tranchant <alexandre.tranchant@gmail.com>
+ *
+ * @author    Alexandre Tranchant <alexandre.tranchant@gmail.com>
+ * @copyright 2019 Alexandre Tranchant
+ * @license   Cecill-B http://www.cecill.info/licences/Licence_CeCILL-B_V1-fr.txt
+ */
+
+declare(strict_types=1);
 
 namespace App;
 
+use Exception;
 use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
+use Symfony\Component\Config\Exception\LoaderLoadException;
 use Symfony\Component\Config\Loader\LoaderInterface;
 use Symfony\Component\Config\Resource\FileResource;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Kernel as BaseKernel;
 use Symfony\Component\Routing\RouteCollectionBuilder;
 
+/**
+ * Kernel class.
+ *
+ * Register bundles, configure routes and containers.
+ */
 class Kernel extends BaseKernel
 {
     use MicroKernelTrait;
 
     private const CONFIG_EXTS = '.{php,xml,yaml,yml}';
 
+    /**
+     * Register all bundles declared in bundles.php file.
+     *
+     * @return iterable
+     */
     public function registerBundles(): iterable
     {
         $contents = require $this->getProjectDir().'/config/bundles.php';
@@ -25,6 +50,14 @@ class Kernel extends BaseKernel
         }
     }
 
+    /**
+     * Configure all containers defined.
+     *
+     * @param ContainerBuilder $container the container builder
+     * @param LoaderInterface  $loader    the loader
+     *
+     * @throws Exception if something went wrong when loading resource
+     */
     protected function configureContainer(ContainerBuilder $container, LoaderInterface $loader): void
     {
         $container->addResource(new FileResource($this->getProjectDir().'/config/bundles.php'));
@@ -37,6 +70,13 @@ class Kernel extends BaseKernel
         $loader->load($confDir.'/{services}_'.$this->environment.self::CONFIG_EXTS, 'glob');
     }
 
+    /**
+     * Configure routes.
+     *
+     * @param RouteCollectionBuilder $routes the route collection builder
+     *
+     * @throws LoaderLoadException if something went wrong during import
+     */
     protected function configureRoutes(RouteCollectionBuilder $routes): void
     {
         $confDir = $this->getProjectDir().'/config';
