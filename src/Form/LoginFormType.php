acc725a4cfca8146391301710caf45f2fca02f22
Alexandre-T <alexandre.tranchant@gmail.com>
2019-04-03 22:04:09 +0200

    Removing duplicate header

--- src/Form/LoginFormType.php
+++ src/Form/LoginFormType.php
@@ -12,21 +12,6 @@
  */
 
 declare(strict_types=1);
-/**
- * This file is part of the Contact Application.
- *
- * PHP version 7.2
- *
- * (c) Alexandre Tranchant <alexandre.tranchant@gmail.com>
- *
- * @category Entity
- *
- * @author    Alexandre Tranchant <alexandre.tranchant@gmail.com>
- * @copyright 2019 Cerema
- * @license   CeCILL-B V1
- *
- * @see       http://www.cecill.info/licences/Licence_CeCILL-B_V1-en.txt
- */
 
 namespace App\Form;
 
