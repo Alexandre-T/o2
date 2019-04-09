ff88acb0b72d683d0f5d0eff84afc15e27c4e906
Alexandre-T <alexandre.tranchant@gmail.com>
2019-04-09 10:26:27 +0200

    Gedmo activated.

---
+++ src/Migrations/Version20190409082355.php
@@ -0,0 +1,71 @@
+<?php
+/**
+ * This file is part of the O2 Application.
+ *
+ * PHP version 7.1|7.2|7.3|7.4
+ *
+ * (c) Alexandre Tranchant <alexandre.tranchant@gmail.com>
+ *
+ * @author    Alexandre Tranchant <alexandre.tranchant@gmail.com>
+ * @copyright 2019 Alexandre Tranchant
+ * @license   Cecill-B http://www.cecill.info/licences/Licence_CeCILL-B_V1-fr.txt
+ */
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\DBALException;
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Gedmo log entries.
+ */
+final class Version20190409082355 extends AbstractMigration
+{
+    /**
+     * Description getter.
+     *
+     * @return string
+     */
+    public function getDescription(): string
+    {
+        return 'Gedmo log entries';
+    }
+
+    /**
+     * Create gedmo log entries table.
+     *
+     * @param Schema $schema
+     *
+     * @throws DBALException
+     */
+    public function up(Schema $schema): void
+    {
+        $this->abortIf('postgresql' !== $this->connection->getDatabasePlatform()->getName(), 'Migration can only be executed safely on \'postgresql\'.');
+
+        $this->addSql('CREATE SEQUENCE ext_log_entries_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
+        $this->addSql('CREATE TABLE ext_log_entries (id INT NOT NULL, action VARCHAR(8) NOT NULL, logged_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, object_id VARCHAR(64) DEFAULT NULL, object_class VARCHAR(255) NOT NULL, version INT NOT NULL, data TEXT DEFAULT NULL, username VARCHAR(255) DEFAULT NULL, PRIMARY KEY(id))');
+        $this->addSql('CREATE INDEX log_class_lookup_idx ON ext_log_entries (object_class)');
+        $this->addSql('CREATE INDEX log_date_lookup_idx ON ext_log_entries (logged_at)');
+        $this->addSql('CREATE INDEX log_user_lookup_idx ON ext_log_entries (username)');
+        $this->addSql('CREATE INDEX log_version_lookup_idx ON ext_log_entries (object_id, object_class, version)');
+        $this->addSql('COMMENT ON COLUMN ext_log_entries.data IS \'(DC2Type:array)\'');
+    }
+
+    /**
+     * Drop gedmo log entries table.
+     *
+     * @param Schema $schema
+     *
+     * @throws DBALException
+     */
+    public function down(Schema $schema): void
+    {
+        $this->abortIf('postgresql' !== $this->connection->getDatabasePlatform()->getName(), 'Migration can only be executed safely on \'postgresql\'.');
+
+        $this->addSql('DROP SEQUENCE ext_log_entries_id_seq CASCADE');
+        $this->addSql('DROP TABLE ext_log_entries');
+    }
+}
