97df9f14578987c08178954e2ee9ff7289876c42
Alexandre-T <alexandre.tranchant@gmail.com>
2019-05-12 21:10:18 +0200

    Programmation acceptance test improved.

--- tests/acceptance/ProgrammationCest.php
+++ tests/acceptance/ProgrammationCest.php
@@ -27,19 +27,6 @@ class ProgrammationCest
      *
      * @param AcceptanceTester $you acceptance tester
      */
-    public function tryToListNonExistentProgrammation(AcceptanceTester $you): void
-    {
-        $you->wantTo('list an empty programmation');
-        $you->login('customer-7');
-        $you->amOnPage('/customer/programmation/list');
-        $you->seeResponseCodeIsSuccessful();
-        $you->see('Vous n’avez pas encore demandé de reprogrammation');
-    }
-    /**
-     * You try to list your programmation.
-     *
-     * @param AcceptanceTester $you acceptance tester
-     */
     public function tryListAndShowProgrammation(AcceptanceTester $you): void
     {
         $you->wantTo('list a non-empty programmation');
@@ -48,14 +35,14 @@ class ProgrammationCest
         $you->seeResponseCodeIsSuccessful();
         $you->dontSee('Vous n’avez pas encore demandé de reprogrammation');
         $you->see('Make1');
-        $you->click('Consulter','a.btn');
+        $you->click('Consulter', 'a.btn');
         $you->seeResponseCodeIsSuccessful();
         $identifier = $you->grabFromCurrentUrl('~^\/customer\/programmation\/(\d+)$~');
         $you->seeCurrentUrlEquals('/customer/programmation/'.$identifier);
         $you->see('Votre reprogrammation est en cours. Notre équipe travaille dessus');
         $you->click('Déconnexion', 'nav');
         $you->login('programmer');
-        $you->click('Fichiers en cours','nav');
+        $you->click('Fichiers en cours', 'nav');
         $you->seeResponseCodeIsSuccessful();
         $you->seeCurrentUrlEquals('/programmer/list');
         $you->click('Télécharger', 'a.btn');
@@ -63,5 +50,38 @@ class ProgrammationCest
         $programIdentifier = $you->grabFromCurrentUrl('~^/programmer/download-original/(\d+)$~');
         $you->seeCurrentUrlEquals('/programmer/download-original/'.$programIdentifier);
         $you->see('This file is used to be uploaded');
+
+        $you->areOnPage('/programmer/'.$programIdentifier);
+        $you->seeResponseCodeIsSuccessful();
+        $you->see('Le client est en attente de son fichier de reprogrammation');
+        $you->seeLink('Télécharger la programmation initiale');
+        $you->click('Nouvelle cartographie', 'a.btn-primary');
+        $you->seeResponseCodeIsSuccessful();
+        $you->seeCurrentUrlEquals('/programmer/upload/'.$programIdentifier);
+        $you->fillField('Commentaire pour le client', 'Petite phrase pour le client.');
+        $you->attachFile('upload_programmation_form[finalFile][file]', 'upload.txt');
+        $you->selectOption('upload_programmation_form[edcStopped]', '1');
+        $you->click('Envoyer la reprogrammation au client');
+        $you->seeResponseCodeIsSuccessful();
+        $you->seeCurrentUrlEquals('/programmer/'.$programIdentifier);
+        $you->see('La reprogrammation est terminée. Le client peut télécharger sa nouvelle cartographie');
+        $you->click('Télécharger la reprogrammation');
+        $you->seeResponseCodeIsSuccessful();
+        $you->seeCurrentUrlEquals('/programmer/download-final/'.$programIdentifier);
+        $you->see('This file is used to be uploaded');
+    }
+
+    /**
+     * You try to list your programmation.
+     *
+     * @param AcceptanceTester $you acceptance tester
+     */
+    public function tryToListNonExistentProgrammation(AcceptanceTester $you): void
+    {
+        $you->wantTo('list an empty programmation');
+        $you->login('customer-7');
+        $you->amOnPage('/customer/programmation/list');
+        $you->seeResponseCodeIsSuccessful();
+        $you->see('Vous n’avez pas encore demandé de reprogrammation');
     }
 }
